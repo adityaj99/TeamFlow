2588b75af0c3d025710034256fbbe9f71968f51b
adityaj99 <jadhavaditya102@gmail.com>
2026-04-05 21:48:10 +0530

    feat: add auth middleware to protect routes using JWT and cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 import userRoutes from "./modules/user/user.routes.js";
+import { protect } from "./middlewares/auth.middleware.js";
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,10 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Teamflow is running!");
+});
+
+app.get("/api/protected", protect, (req, res) => {
+  res.send("This is a protected route. User ID: " + req.user._id);
 });
 
 app.use("/api/auth", userRoutes);
