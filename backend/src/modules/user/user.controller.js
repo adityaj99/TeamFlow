aba3f7cd93223d90be20b3befa4681c4cce69dfa
adityaj99 <jadhavaditya102@gmail.com>
2026-04-14 11:31:21 +0530

    fix: update cookie settings in login response for improved security and compatibility

--- backend/src/modules/user/user.controller.js
+++ backend/src/modules/user/user.controller.js
@@ -33,8 +33,8 @@ const login = async (req, res, next) => {
 
     res.cookie("tf_token", token, {
       httpOnly: true,
-      secure: false || process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
