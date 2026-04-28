a733f903e966c9421990f5c1450a8bc2dd95c691
adityaj99 <jadhavaditya102@gmail.com>
2026-04-28 21:12:39 +0530

    feat(cors): update CORS configuration to support multiple origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ import { limiter } from "./middlewares/rateLimiter.js";
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   }),
 );
